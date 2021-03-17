849b7cf2c5dcc527144e920d4c1207dbb74d3e8d
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-03-17 09:44:04 +0100

    add test class

---
+++ piazza/src/main/java/piazza/Test.java
@@ -0,0 +1,7 @@
+package main.java.piazza;
+
+public class Test {
+    public void displayMessage() { // removed semi colon from your code
+        System.out.println("Welcome to the Grade book!"); // added semi colon to your code
+    }
+}
