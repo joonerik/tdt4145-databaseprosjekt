68000030ac38b4bbd780b9b5bbcb8e1277f1bf58
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-03-17 09:10:09 +0100

    add main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package Main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+
+        TestController = new DBConn();
+        TestController.connect();
+
+    }
+}
