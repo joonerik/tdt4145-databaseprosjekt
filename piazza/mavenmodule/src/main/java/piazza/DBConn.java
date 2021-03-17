eb482c2fd379af14c0f33ac7b3b53fda0c959bf2
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-03-17 10:48:13 +0100

    add query for user

--- piazza/mavenmodule/src/main/java/piazza/DBConn.java
+++ piazza/mavenmodule/src/main/java/piazza/DBConn.java
@@ -9,14 +9,20 @@ public class DBConn {
     }
     public void connect() {
         try {
-            // Class.forName("com.mysql.jdbc.Driver").newInstance(); when you are using MySQL 5.7
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Properties for user and password.
             Properties p = new Properties();
             p.put("user", "root");
             p.put("password", "database123");
-            //            conn = DriverManager.getConnection("jdbc:mgit pysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/testDB?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false",p);
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database",p);
+
+            Statement stmt= conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from User1");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+            conn.close();
+
         } catch (Exception e)
         {
             throw new RuntimeException("Unable to connect", e);
